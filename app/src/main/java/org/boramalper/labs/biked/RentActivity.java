74ab6ce2fcec271101984c02baf432da06f81e24
Bora M. Alper <bora@boramalper.org>
2017-10-07 13:53:22 +0100

    commit before changing explore

--- app/src/main/java/org/boramalper/labs/biked/RentActivity.java
+++ app/src/main/java/org/boramalper/labs/biked/RentActivity.java
@@ -1,11 +1,24 @@
 package org.boramalper.labs.biked;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class RentActivity extends AppCompatActivity {
 
@@ -30,6 +43,82 @@ public class RentActivity extends AppCompatActivity {
                 }
 
                 return true;
+            }
+        });
+
+        findViewById(R.id.buttonCall).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+441315565560"));
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.editTextDateFrom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText editTextDateFrom = (EditText) findViewById(R.id.editTextDateFrom);
+                String dateString = editTextDateFrom.getText().toString();
+                // TODO: Adjust according to today
+                int year = 2017, month = 10 - 1, day = 8;
+                if (dateString.length() == 10) {
+                    Scanner scanner = new Scanner(dateString);
+                    scanner.useDelimiter("-");
+                    year = scanner.nextInt();
+                    month = scanner.nextInt() - 1;
+                    day = scanner.nextInt();
+                }
+
+                new DatePickerDialog(
+                        v.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                editTextDateFrom.setText(String.format(Locale.US, "%4d-%02d-%02d", year, month + 1, dayOfMonth));
+                            }
+                        },
+                        year,
+                        month,
+                        day
+                ).show();
+            }
+        });
+
+        findViewById(R.id.editTextDateTo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText editTextDateTo = (EditText) findViewById(R.id.editTextDateTo);
+                String dateString = editTextDateTo.getText().toString();
+                // TODO: Adjust according to today
+                int year = 2017, month = 10 - 1, day = 8;
+                if (dateString.length() == 10) {
+                    Scanner scanner = new Scanner(dateString);
+                    scanner.useDelimiter("-");
+                    year = scanner.nextInt();
+                    month = scanner.nextInt() - 1;
+                    day = scanner.nextInt();
+                }
+
+                new DatePickerDialog(
+                        v.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                editTextDateTo.setText(String.format(Locale.US, "%4d-%02d-%02d", year, month + 1, dayOfMonth));
+                            }
+                        },
+                        year,
+                        month,
+                        day
+                ).show();
+            }
+        });
+
+        findViewById(R.id.buttonSearch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                proceedTo(RentSearchResultsActivity.class, false);
             }
         });
     }
