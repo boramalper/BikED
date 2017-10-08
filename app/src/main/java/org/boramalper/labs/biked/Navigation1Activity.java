342d0305d6b664963721260c182ba8fa53fdc316
Bora M. Alper <bora@boramalper.org>
2017-10-08 11:24:41 +0100

    added real location to navigation

--- app/src/main/java/org/boramalper/labs/biked/Navigation1Activity.java
+++ app/src/main/java/org/boramalper/labs/biked/Navigation1Activity.java
@@ -1,6 +1,9 @@
 package org.boramalper.labs.biked;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,13 +18,13 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.maps.android.data.kml.KmlLayer;
 
 public class Navigation1Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation1);
         mMapView = (MapView) findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
+        final Navigation1Activity nav1Act = this;
         mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
@@ -39,6 +42,20 @@ public class Navigation1Activity extends AppCompatActivity {
                 }
 
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(55.947181, -3.201047), 18));
+                if (ActivityCompat.checkSelfPermission(nav1Act, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(nav1Act, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    ActivityCompat.requestPermissions(nav1Act, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 31);
+
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+
             }
         });
     }
